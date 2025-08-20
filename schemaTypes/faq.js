cf9539863c61dea454c4b05fe747a7237bcbd2fb
Eric Streske <eric@Erics-MacBook-Air.local>
2025-08-20 09:12:22 -0700

    new FAQ schema

--- schemaTypes/faq.js
+++ schemaTypes/faq.js
@@ -1,19 +1,30 @@
 export default {
-    name: 'faq',
-    type: 'document',
-    title: 'FAQ',
-    fields: [
-      {
-        name: 'question',
-        type: 'string',
-        title: 'Question',
-        validation: Rule => Rule.required(),
-      },
-      {
-        name: 'answer',
-        type: 'text',
-        title: 'Answer',
-        validation: Rule => Rule.required(),
-      },
-    ]
-  };
+  name: 'faq',
+  type: 'document',
+  title: 'FAQ',
+  fields: [
+    {
+      name: 'question',
+      type: 'string',
+      title: 'Question',
+      validation: Rule => Rule.required(),
+    },
+    {
+      name: 'answer',
+      type: 'array',
+      title: 'Answer',
+      of: [
+        {
+          type: 'block',
+        },
+        {
+          type: 'list',
+          name: 'bulletList',
+          title: 'Bulleted List',
+          of: [{ type: 'string' }],
+        },
+      ],
+      validation: Rule => Rule.required(),
+    },
+  ],
+};
